f544534cb7ea66cd73fd3172d151b5fa15778f6a
Ricardo Rocha <ricardorochaa49@gmail.com>
2026-08-05 23:48:35 +0100

    ok

--- gaussian_process.py
+++ gaussian_process.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def kernel_function(X: np.ndarray, l: float, sigma: float) -> np.ndarray:
     "Computes the covariance matrix using the squared exponential"
-
-    x = X[0:-1]
-    xl = X[1:]
-    K = sigma**2 * np.exp(-0.5 * np.transpose(x - xl) * l**(-2) * (x - xl))
+    K = np.zeros((np.size(X),np.size(X)))
+    for i in range(np.size(X)):
+        x = X[i] * np.ones(np.size(X))
+        K[i,:] = sigma**2 * np.exp(-0.5 * np.transpose(x - X) * l**(-2) * (x - X))
     return K
     
 
@@ -19,6 +19,7 @@ Xtest = np.linspace(-5,5,10)
 l = 1
 sigma = 0.5
 
-kernel = kernel_function(Xtest, l, sigma)
-print(kernel)
+covariance = kernel_function(Xtest, l, sigma)
+
+
 
